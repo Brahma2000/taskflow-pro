75fc0f9d880e7c7c1248dbc2668ac0fecb2db5db
Brahma Teja Gundapaneni <brahmatejagundapaneni@gmail.com>
2026-02-12 15:03:19 -0600

    Create auth_routes.py
    Creating authentication routes

---
+++ app/routes/auth_routes.py
@@ -0,0 +1,31 @@
+from flask import Blueprint, request, jsonify
+from app.extensions import db
+from app.models.user import User
+from flask_jwt_extended import create_access_token
+
+auth_bp = Blueprint("auth", __name__)
+
+@auth_bp.route("/register", methods=["post"])
+def register():
+  data = request.get_json()
+
+  user = User(username=data["username'])
+  user.set_password(data["password"])
+
+  db.session.add(user)
+  db.session.commit()
+
+  return jsonify({"message": "User registered"}), 201
+
+@auth_bp.route("/login", methods=["POST"])
+def login():
+  data = request.get_json()
+
+  user = User.query.filter_by(username=data["username"]).first()
+
+  if not user or not user.check_password(data["password"]):
+    return jsonify({"error": "Invalid credentials"}), 401
+
+  token = create_access_token(identify=user.id)
+
+  return jsonify(access_token=token)
