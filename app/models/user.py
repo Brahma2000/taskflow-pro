557854b18f4c24d9fd1f26a41690d5055b6a014d
Brahma Teja Gundapaneni <brahmatejagundapaneni@gmail.com>
2026-02-16 23:49:09 -0600

    Update user.py

--- app/models/user.py
+++ app/models/user.py
@@ -1,13 +1,12 @@
-from app.extensions import db
-from passlib.hash import bcrypt
+from app.extensions import db, bcrypt
 
 class User(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   username = db.Column(db.String(100), unique=True, nullable=False)
-  password = db.Column(db.String(255), nullable=False)
+  password_hash = db.Column(db.String(255), nullable=False)
 
   def set_password(self, password):
-    self.password = bcrypt.hash(password)
+    self.password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
 
   def check_password(self, password):
-    return bcrypt.verify(password, self.password)
+    return bcrypt.check_password_hash(self.password_hash, password)
